3c38ea0fd47de2145b1ac6a02b976d55a18eabde
HHX0924 <2697430277@qq.com>
2023-07-10 16:37:50 +0800

    echart组件封装 筛选区结构搭建 table列表区域结构搭建 频道列表渲染

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,4 +1,159 @@
-function Article () {
-    return <div>Article</div>
+import { Link } from 'react-router-dom'
+import {
+    Card,
+    Breadcrumb,
+    Form,
+    Button,
+    Radio,
+    DatePicker,
+    Select,
+    Table,
+    Tag,
+    Space } from 'antd'
+import './index.scss'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import img404 from '@/assets/error.png'
+import {useEffect, useState} from "react";
+import {http} from "@/utils/idnex";
+const { Option } = Select
+const { RangePicker } = DatePicker
+
+const Article = () => {
+    const [channelList, setChannelList] = useState([])
+    const loadChannelList = async () => {
+        const res = await http.get('/channels')
+        setChannelList(res.data.channels)
+    }
+    useEffect(() => {
+        loadChannelList()
+    },[])
+    const onFinish = (values) => {
+
+        console.log(values)
+    }
+    const columns = [
+        {
+            title: '封面',
+            dataIndex: 'cover',
+            width:120,
+            render: cover => {
+                return <img src={cover || img404} width={80} height={60} alt="" />
+            }
+        },
+        {
+            title: '标题',
+            dataIndex: 'title',
+            width: 220
+        },
+        {
+            title: '状态',
+            dataIndex: 'status',
+            render: data => <Tag color="green">审核通过</Tag>
+        },
+        {
+            title: '发布时间',
+            dataIndex: 'pubdate'
+        },
+        {
+            title: '阅读数',
+            dataIndex: 'read_count'
+        },
+        {
+            title: '评论数',
+            dataIndex: 'comment_count'
+        },
+        {
+            title: '点赞数',
+            dataIndex: 'like_count'
+        },
+        {
+            title: '操作',
+            render: data => {
+                return (
+                    <Space size="middle">
+                        <Button type="primary" shape="circle" icon={<EditOutlined />} />
+                        <Button
+                            type="primary"
+                            danger
+                            shape="circle"
+                            icon={<DeleteOutlined />}
+                        />
+                    </Space>
+                )
+            }
+        }
+    ]
+    const data = [
+        {
+            id: '8218',
+            comment_count: 0,
+            cover: {
+                images:['http://geek.itheima.net/resources/images/15.jpg'],
+            },
+            like_count: 0,
+            pubdate: '2019-03-11 09:00:00',
+            read_count: 2,
+            status: 2,
+            title: 'wkwebview离线化加载h5资源解决方案'
+        }
+    ]
+    return (
+        <div>
+            {/*筛选区域*/}
+            <Card
+                title={
+                    <Breadcrumb separator=">">
+                        <Breadcrumb.Item>
+                            <Link to="/home">首页</Link>
+                        </Breadcrumb.Item>
+                        <Breadcrumb.Item>内容管理</Breadcrumb.Item>
+                    </Breadcrumb>
+                }
+                style={{ marginBottom: 20 }}
+            >
+                <Form
+                    onFinish={onFinish}
+                    initialValues={{ status: null }}>
+                    <Form.Item label="状态" name="status">
+                        <Radio.Group>
+                            <Radio value={null}>全部</Radio>
+                            <Radio value={0}>草稿</Radio>
+                            <Radio value={1}>待审核</Radio>
+                            <Radio value={2}>审核通过</Radio>
+                            <Radio value={3}>审核失败</Radio>
+                        </Radio.Group>
+                    </Form.Item>
+
+                    <Form.Item label="频道" name="channel_id">
+                        <Select
+                            placeholder="请选择文章频道"
+                            style={{ width: 120 }}
+                        >
+                            {channelList.map(item =>
+                                <Option
+                                key={item.id}
+                                value={item.id}>{item.name}</Option>)}
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item label="日期" name="date">
+                        <RangePicker />
+                    </Form.Item>
+
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" style={{ marginLeft: 80 }}>
+                            筛选
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+            {/*文章列表区域*/}
+            <Card title={`根据筛选条件共查询到 count 条结果：`}>
+                <Table rowKey="id" columns={columns} dataSource={data} />
+            </Card>
+        </div>
+    )
 }
+
 export default Article
